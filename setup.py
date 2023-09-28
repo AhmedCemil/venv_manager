23b57fd744fc08541e9a2cc76c76fcf8a225c495
Ahmed Cemil BİLGİN <70539255+AhmedCemil@users.noreply.github.com>
2023-09-28 10:55:46 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='venv_manager',
-    version='0.1.0',
+    version='0.1',
     description='A Python script to automate the creation and management of Python virtual environments.',
     license='MIT',
-    author='Ahmed Cemil',
-    author_email='ahmedcemil@example.com',
+    author='Ahmed Cemil Bilgin',
+    author_email='ahmed.c.bilgin@gmail.com',
     url='https://github.com/AhmedCemil/venv_manager',
     packages=['venv_manager'],
     entry_points={
