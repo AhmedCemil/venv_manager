250bbb3dba93c3719c843ffefaee2e05a0036c51
Ahmed Cemil BİLGİN <70539255+AhmedCemil@users.noreply.github.com>
2023-09-28 10:46:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='acb_venv_manager',
+    name='venv_manager',
     version='0.1.0',
     description='A Python script to automate the creation and management of Python virtual environments.',
     license='MIT',
     author='Ahmed Cemil',
     author_email='ahmedcemil@example.com',
     url='https://github.com/AhmedCemil/venv_manager',
-    packages=['acb_venv_manager'],
+    packages=['venv_manager'],
     entry_points={
         'console_scripts': [
             'acb_venv_manager = acb_venv_manager.venv_manager:__main__',
